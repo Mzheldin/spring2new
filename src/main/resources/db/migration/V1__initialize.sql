9fa6fbd3aecbdd9ff26bb3187a879e6807782343
Mzheldin <mzheldin@yandex.ru>
2019-10-08 14:02:46 +0300

    homework 2

--- src/main/resources/db/migration/V1__initialize.sql
+++ src/main/resources/db/migration/V1__initialize.sql
@@ -6,7 +6,7 @@ CREATE TABLE users (
   first_name            VARCHAR(50) NOT NULL,
   last_name             VARCHAR(50) NOT NULL,
   email                 VARCHAR(50) NOT NULL,
-  phone                 VARCHAR(15) NOT NULL,
+  phone                 VARCHAR(20) NOT NULL,
   PRIMARY KEY (id)
 );
 
